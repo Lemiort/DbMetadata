e07d7b7c2b9411ebbdd6455b386f20f3a785aaf0
Lemiort <lemiort@gmail.com>
2017-12-12 23:50:51 +0300

    Добавьте файлы проекта.

---
+++ DbMetadata/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DbMetadata.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
